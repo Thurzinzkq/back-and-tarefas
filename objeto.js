14bb79ca76e763766661fbcdedfab25ecd1d5245
Thurzinzkq <alves.mello.arthur@escola.pr.gov.br>
2026-04-27 11:53:19 +0000

    v1

---
+++ objeto.js
@@ -0,0 +1,15 @@
+const bancoAutomatico = {
+    material: "Couro Natural",
+    Cor: "Caramelo",
+    Tamanho: "Padrão",
+    Concluido: false
+}
+
+//console.log("Material escolhido: ", bancoAutomatico.material);
+
+
+//bancoAutomatico.Concluido = true;
+//console.log(bancoAutomatico);
+const papelaria = {
+    material: ""
+}
